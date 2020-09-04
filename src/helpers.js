07ba780a79f7ed311c56b749cb01a8cc0c2a6329
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-04 17:46:32 -0500

    refactor: Moved map functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,6 @@
 import {electoralCollege, db} from './testDb';
+import {RepublicanRed, DemocratBlue, BattlegroundPurple} from './Global';
+
 
 export function addVote(party, candidate, state) {
   // party
@@ -22,7 +24,7 @@ export function addVote(party, candidate, state) {
   
   // total
   db.total++;
-  console.log(db);
+  // console.log(db);
 }
 
 export function returnElectoralVotes() {
@@ -47,3 +49,39 @@ export function returnElectoralVotes() {
   db.electoralCollege.biden = bidenEV;
   db.electoralCollege.trump = trumpEV;
 }
+
+export const mapHandler = (event, setState) => {
+  const state = event.target.dataset.name;
+  setState(state);
+};
+
+const returnMapColor = (biden, trump) => {
+  if(biden > trump) return DemocratBlue;
+  if(trump > biden) return RepublicanRed;
+  return BattlegroundPurple;
+}
+
+export const statesCustomConfig = () => {
+  let map = {
+    // NJ: {
+    //   fill: "navy",
+    //   clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
+    // },
+    // NY: {
+    //   fill: "#CC0000"
+    // }
+  };
+
+  // loop through states
+  for(const [state, value] of Object.entries(db.states)) {
+
+    const biden = value.biden ? value.biden.total : 0;
+    const trump = value.trump ? value.trump.total : 0;
+    
+    map[state] = {
+      fill: returnMapColor(biden, trump),
+    };
+  }
+  
+  return map;
+};
