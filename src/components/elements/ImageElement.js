1002159140b9f188a7128f810f47d52591d6efdf
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-13 16:31:50 -0500

    feat: Added context to update user selection

--- src/components/elements/ImageElement.js
+++ src/components/elements/ImageElement.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components';
 
+import {VoteContext} from '../context'
+
 const ImageContainer = styled.div`
   ${'' /* width: 50%; */}
+  h3 {
+    margin-bottom: 25px;
+  }
 `;
 
 const Image = styled.img`
@@ -11,6 +16,7 @@ const Image = styled.img`
   filter: grayscale(.4);
   border-radius: 10px;
   width: 70%;
+  cursor: pointer;
   :hover {
     transform: scale(1.02,1.02);
     filter: grayscale(0);
@@ -19,11 +25,18 @@ const Image = styled.img`
 `;
 
 const ImageElement = props => {
-  const {img, alt, title, color} = props;
+  const {img, alt, title, color, type, choice} = props;
+  
+  const [vote, setVote] = useContext(VoteContext);
 
+  const onClickHandler = (event, type, choice) => {
+    // event.preventDefault();
+    setVote(prevVote => ({...prevVote, [type]: choice}))
+  }
+  
   return (
     <div>
-       <ImageContainer>
+       <ImageContainer onClick={(event) => onClickHandler(event, type, choice)}>
           <h3>{title}</h3>
           <Image src={require(`../../images/${img}`)} alt={alt} color={color} />
         </ImageContainer> 
@@ -36,6 +49,8 @@ ImageElement.propTypes = {
   alt: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   color: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  choice: PropTypes.string.isRequired,
 }
 
 export default ImageElement
