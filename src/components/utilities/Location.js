693a3a5f0dcbcad8f245c89f07fd07fee26bc383
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-13 16:31:04 -0500

    feat: Added Location component to fetch user location

---
+++ src/components/utilities/Location.js
@@ -0,0 +1,52 @@
+// function getCurrentLocation(options) {
+//   return new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(resolve, ({code, message}) =>
+//       reject(Object.assign(new Error(message), {name: "PositionError", code})),
+//       options);
+//     });
+// };
+
+// export async function inout() {
+//   try {
+//     console.log(await getCurrentLocation({
+//       enableHighAccuracy: true,
+//       timeout: 5000,
+//       maximumAge: 0
+//     }));
+//   } catch (e) {
+//     if (e.name === 'PositionError') {
+//       console.log(e.message + ". code = " + e.code);
+//     }
+//   }
+// }
+
+// inout().catch(e => console.log(e)); // User denied geolocation prompt. code = 1
+
+export async function setLocation(setVote, setErr) {
+  const region = await fetchLocation(setErr);
+  setVote(prevVote => ({...prevVote, state: region}));
+}
+
+async function fetchLocation(setErr) {
+  const url = 'https://api.ipdata.co/?api-key=cea6616cd82cda5922f631bdbca05f3936a4f66fba207920db9a02f4';
+  // const ress = await fetch(url)
+  //   .then(res => {
+  //     console.log(res);
+  //     return res.json();
+  //   })
+  //   .then(data => {
+  //     console.log(data);
+  //   });
+  const res = await fetch(url);
+  const results = await res.json();
+  if(res.status === 200){
+    // console.log(results);
+    // console.log(results.region_code);
+    // setErr(true);
+    return results.region_code;
+  } else {
+    console.log(results);
+    console.log(results.message)
+    setErr(true);
+  }
+}
