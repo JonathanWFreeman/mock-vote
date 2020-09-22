ad7a9c5b7ce63fd7d969cd7f330a2fe263ec69bc
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-21 22:06:33 -0500

    feat: Added firebase submission

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -2,26 +2,31 @@ import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
 // import PropTypes from 'prop-types'
 
-import {addVote} from '../helpers';
+import {addVote, setFirebaseData} from '../helpers';
 import {VoteContext, DataContext} from './context'
+import { useFirebase } from './firebase'
 
-function handleSubmit(data, vote) {
-  addVote(vote);
+function handleSubmit(firebase, vote) {
+  setFirebaseData(firebase, vote);
   console.log('works');
-  console.log(data);
+  // addVote(vote);
 }
 
 const Submit = () => {
   const [vote] = useContext(VoteContext);
   const [data, setData] = useContext(DataContext);
+  const firebase = useFirebase();
+
+  console.log(firebase);
   console.log(vote)
   console.log(data)
+
   return (
     <div>
       <p>{vote && vote.state}</p>
       <p>{vote && vote.candidate}</p>
       <p>{vote && vote.party}</p>
-      <button onClick={() => handleSubmit(data, vote)}><Link to='/results'>SUBMIT</Link></button>
+      <button onClick={() => handleSubmit(firebase, vote)}><Link to='/results'>SUBMIT</Link></button>
       <button><Link to='/candidate'>RESET</Link></button>
     </div>
   )
