af237c2f45f42227ff35afd3a2bddd2236048112
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-21 22:08:23 -0500

    refactor: Added results from firebase,

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useEffect, useRef} from 'react';
+import { useFirebase } from './firebase'
 import styled from 'styled-components';
 import USAMap from 'react-usa-map';
-import {db} from '../testDb';
-import {returnElectoralVotes, mapHandler, statesCustomConfig} from '../helpers';
-import {useWindowDimensions, StateData} from './utilities'
+import {returnElectoralVotes, mapHandler, statesCustomConfig, getFirebaseData, isEmpty} from '../helpers';
+import {useWindowDimensions, Below} from './utilities'
+import {StateData} from './elements'
+import {DataContext} from './context'
 // import PropTypes from 'prop-types'
 
 const ResultDiv = styled.div`
@@ -13,7 +15,7 @@ const ResultDiv = styled.div`
 `;
 
 const MapSection = styled.section`
-  margin: 5% 0 0;
+  margin: 5% 0;
   path {
     pointer-events: all;
   }
@@ -23,6 +25,9 @@ const MapSection = styled.section`
   }
   svg {
     width: 100%;
+  }
+  h3 {
+    margin-bottom: 2%;
   }
 `;
 
@@ -39,48 +44,117 @@ const StateResults = styled.section`
 
 const CandidateResults = styled.section`
   display: flex;
-  display: flex;
   justify-content: space-evenly;
   width: 100%;
 `;
 
-const Results = (props, event) => {
-  
-  const [state, setState] = useState('');
-  const { width } = useWindowDimensions();
-  returnElectoralVotes();
+const BreakdownResults = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  flex-direction: column;
+  text-transform: capitalize;
+  div {
+    margin: 1% 0;
+    ${Below.small`
+      display: block;
+    `}
+  }
+`;
 
+
+
+const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
+const Results = () => {
+  // const [db, setDb] = useContext(DataContext);
+  const [state, setState] = useState('');
+  const [mapColor, setMapColor] = useState({});
+  const { width } = useWindowDimensions();
+  // write async function wait for data - loader
+  
+  const [db, setDb] = useState({});
+  const firebase = useFirebase();
+  const {current:type} = useRef(['parties', 'candidates', 'states', 'total', 'electoralCollege'])
+  
+  useEffect(() => {
+    async function doStuff(){
+      await getFirebaseData(firebase, type, setDb);
+      await wait(2000);
+    }
+    doStuff();
+  }, [firebase, type])
+  
+  const ec = returnElectoralVotes(db);
   return (
     <>
-      <h2>Results</h2>
-      <CandidateResults>
-        <div>
-          <h2>Popular</h2>
-          <ResultDiv>
-            <h3>Biden: {db.candidates.biden.total}</h3>
-          </ResultDiv>
-          <ResultDiv>
-            <h3>Trump: {db.candidates.trump.total}</h3>
-          </ResultDiv>
-        </div>
-        <div>
-        <h2>Electoral</h2>
-          <ResultDiv>
-            <h3>Biden: {db.electoralCollege.biden}</h3>
-          </ResultDiv>
-          <ResultDiv>
-            <h3>Trump: {db.electoralCollege.trump}</h3>
-          </ResultDiv>
-        </div>
-      </CandidateResults>
-      {state ? 
-        <StateResults>
-          <StateData clickedState={state} setState={setState} /> 
-        </StateResults>
-      : null}
-      <MapSection>
-        <USAMap title='US Voting Map' customize={statesCustomConfig()} onClick={(event) => mapHandler(event, setState)} width={width} height={width / 2.5}/>
-      </MapSection>
+      {db.electoralCollege &&
+        <>
+          <h2>Results</h2>
+          <CandidateResults>
+            <div>
+              <h2>Popular</h2>
+              <ResultDiv>
+                <h3>Biden: {db.candidates.biden.total}</h3>
+              </ResultDiv>
+              <ResultDiv>
+                <h3>Trump: {db.candidates.trump.total}</h3>
+              </ResultDiv>
+            </div>
+            <div>
+            <h2>Electoral</h2>
+              <ResultDiv>
+                <h3>Biden: {ec.biden}</h3>
+              </ResultDiv>
+              <ResultDiv>
+                <h3>Trump: {ec.trump}</h3>
+              </ResultDiv>
+            </div>
+          </CandidateResults>
+          {state && state !== 'DC' ? 
+            <StateResults>
+              <StateData clickedState={state} setState={setState} db={db} /> 
+            </StateResults>
+          : null}
+          <MapSection>
+            <h3>Select a state for more information</h3>
+            <USAMap title='US Voting Map' customize={statesCustomConfig(db)} onClick={(event) => mapHandler(event, setState)} width={width} height={width / 2.5}/>
+          </MapSection>
+          <h2>Results Breakdown</h2>
+          <BreakdownResults>
+            <ResultDiv>
+                {Object.entries(db.parties).map(([doc, collection], index) => {
+                  let el = [];
+                  let sortedCollection = Object.entries(collection).sort();
+                  sortedCollection.forEach(([key, value], index) => {
+                    el.push(<h3 key={index}>{key}: {value}</h3>)
+                  })
+                  console.log(el)
+                  return (
+                      <div key={index}>
+                        {el}
+                      </div>
+                    )
+                })}
+              </ResultDiv>
+              <ResultDiv>
+                {Object.entries(db.candidates).map(([doc, collection], index) => {
+                  let el = [];
+                  let sortedCollection = Object.entries(collection).sort();
+                  sortedCollection.forEach(([key, value], index) => {
+                    el.push(<p key={index}>{key}: {value}</p>)
+                  })
+                  console.log(el)
+                  return (
+                      <div key={index}>
+                        <h2>{doc}:</h2>
+                        <div>{el}</div>
+                      </div>
+                    )
+                })}
+              </ResultDiv>
+          </BreakdownResults>
+        </>
+      }
     </>
   )
 }
