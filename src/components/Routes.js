56602cf601c8789138e5ca2726fd7860f896de3c
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-27 22:59:08 -0500

    feat: Added promise to authHandler. Added checks for userExisting, added localstorage checks and set

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,76 +2,94 @@ import React, {useContext, useEffect, useState} from 'react'
 import firebase from 'firebase'
 import {Candidate, Party, Submit, Error, Results, Login} from './pages'
 import {VoteContext, RouteContext} from './context'
-import {setLocation} from './utilities'
-import { useFirebase } from './firebase';
+import {setLocation, fetchLocation} from './utilities'
+import { useFirebase } from './firebase'
+import {checkUserExists, handleError} from '../helpers'
+
 const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
-
-function checkUserExists(firestore, setUserExists, userEmail) {
-  return new Promise((resolve, reject) => {
-    firestore.users().where('email', '==', userEmail).get()
-    .then(function(ref) {
-      ref.forEach(doc => {
-          console.log(doc.id, " => ", doc.data());
-          setUserExists(true);
-          resolve(true);
-        });
-        resolve(false);
-    }).catch(err => {
-      console.error(err);
-      reject();
-    });
-  })
-}
-
-function providerLogin(provider, firestore, authHandler) {
-  const authProvider = new firebase.auth[`${provider}AuthProvider`]();
-  firestore.auth().signInWithPopup(authProvider).then(authHandler);
-}
-
 const Routes = () => {
-  const [vote, setVote] = useContext(VoteContext);
-  const [country, setCountry] = useState(null);
-  const [err, setErr] =  useState(false);
   const firestore = useFirebase();
+  const [error, setError] =  useState(false);
   const [userExists, setUserExists] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [vote, setVote] = useContext(VoteContext);
   const [route, setRoute] = useContext(RouteContext)
-
-  function authHandler(authData) {
-    // Check if email is in db, if not set email (during submit phase)
-    // set local storage
-    const getUserId = authData.user.uid;
-    const getUserEmail = authData.user.email;
-    // If no email, continue, if email stop
-    console.log(authData);
-    // console.log(getUser);
-    setVote(prevVote => ({...prevVote, uid: getUserId, email: getUserEmail}))
-  }
   
   async function authenticate(provider) {
-    
-    const isUser = await checkUserExists(firestore, setUserExists, 'knokaoutz@gmail.com');
-    // await wait(1000)
-    if(isUser){
-      setRoute('candidate')
-    }else{
-      providerLogin(provider, firestore, authHandler)
-    }
-    // Handle route change if logged in etc
+    await providerLogin(provider);
   }
-  // SET USER INTO VOTE CONTEXT - CAUSE OF BREAKING DURING SUBMISSION
+  
+  async function providerLogin(provider) {
+    const authProvider = await new firebase.auth[`${provider}AuthProvider`]();
+    await firestore.auth().signInWithPopup(authProvider).then(authHandler).catch(err => handleError(err.message, setError, setRoute));
+  }
+  
+  function authHandler(authData) {
+    return new Promise(async (resolve, reject) => {
+      if(authData){
+        console.log(authData);
+        const getUserId = authData.user.uid;
+        const getUserEmail = authData.user.email;
+        const getUserName = authData.user.displayName;
+        // get name
+        // set UID also not just to doc
+                
+        const isUser = await checkUserExists(firestore, getUserId).catch((err) => console.log(err));
+        console.log(isUser);
+        if(isUser === true){
+          setUserExists(true)
+          await wait(3000)
+          setRoute('results')
+          localStorage.setItem('uid', getUserId);
+          setUserExists(false)
+        }else{
+          const userLocation = await fetchLocation().catch((err) => handleError(err, setError, setRoute));
+  
+          if(userLocation){
+            if(userLocation.country_code !== 'US'){
+              setError('US residents only')
+              setRoute('error')
+            }else{
+              setVote(prevVote => ({...prevVote,
+                state: userLocation.region_code,
+                uid: getUserId,
+                email: getUserEmail,
+                name: getUserName,
+              }));       
+              localStorage.setItem('uid', getUserId);
+              setRoute('candidate')
+            }
+          }
+        }
+        // set local storage email or uid idk
+        resolve();
+      }else{
+        reject();
+      }
+    })
+  }
   
   useEffect(() => {
     async function doStuff(){
-      await setLocation(setVote, setErr, setCountry);
-      // const isUser = await checkUserExists(firestore, setUserExists, 'knokoutz@gmail.com'); 
-      // if(isUser){
-      //   setRoute('candidate')
-      // }
+      const isUser = localStorage.getItem('uid');
+      if(isUser){
+        setUserExists(true)
+        await wait(3000)
+        setUserExists(false)
+        setRoute('results')
+      }
     }
     doStuff();
-  }, [firestore, setRoute, setVote, userExists]);
+  }, [setRoute]);
 
-
+  if(userExists) {
+    return (
+      <>
+        <h2>Looks like you've already voted</h2>
+        <h2>You'll be redirected to the results shortly</h2>
+      </>
+    )
+  }
   return (
     <> 
       {route === 'login' && <Login authenticate={authenticate} />}
@@ -79,8 +97,8 @@ const Routes = () => {
       {route === 'party' && <Party />}
       {route === 'submit' && <Submit />}
       {route === 'results' && <Results />}
-      {route === 'error' && <Error country={country} />}
-      {/* {route === 'loading' && <Loading country={country} />} */}
+      {route === 'error' && <Error error={error} />}
+      {/* {route === 'loading' && <Loading />} */}
     </>
   )
 }
