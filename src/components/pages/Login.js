303c3697327fbb5ab211dd56802fde78a5783b2d
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-28 22:20:57 -0500

    refactor: Updated to new component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Button} from '../elements'
 import PropTypes from 'prop-types'
 
 const Login = ({authenticate}) => {
 
   return (
     <>
-      <button onClick={() => authenticate('Facebook')}><p>Login</p></button>
+      <Button onClick={() => authenticate('Facebook')}><p>Login with Facebook</p></Button>
       <p>Only used to help prevent people from voting multiple times.</p>
       <p>Think of it as your voter ID card</p>
     </>
