e190b7539ece2a19eeb52597202072900b06e1a7
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-28 23:22:29 -0500

    fix: Added wrapper for padding on mobile

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import {Button} from '../elements'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {Button} from '../elements'
+
+
+const Wrapper = styled.section`
+  margin: 0 2%;
+`;
 
 const Login = ({authenticate}) => {
-
   return (
     <>
-      <Button onClick={() => authenticate('Facebook')}><p>Login with Facebook</p></Button>
-      <p>Only used to help prevent people from voting multiple times.</p>
-      <p>Think of it as your voter ID card</p>
+      <Wrapper>
+        <Button onClick={() => authenticate('Facebook')}><p>Login with Facebook</p></Button>
+        <p>Only used to help prevent people from voting multiple times.</p>
+        <p>Think of it as your voter ID card</p>
+      </Wrapper>
     </>
   )
 }
