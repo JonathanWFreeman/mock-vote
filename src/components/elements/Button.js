37516e82e36b6e965a05955ed370be97458ef07f
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-28 23:21:39 -0500

    fix: Button padding issues on mobile

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -3,42 +3,38 @@ import PropTypes from 'prop-types';
 import styled, {keyframes} from 'styled-components';
 import {BackgroundColor} from '../../Global'
 
-const rotate = keyframes`
-   from {
-    background-position: 0% 50%;
-  }
-  to {
-    background-position: 100% 50%;
-  }
-`;
+// const rotate = keyframes`
+//    from {
+//     background-position: 0% 50%;
+//   }
+//   to {
+//     background-position: 100% 50%;
+//   }
+// `;
 
-const ButtonGradient = styled.div`
+const ButtonWrapper = styled.div`
+  display: flex;
+  ${'' /* width: 100%; */}
   background: linear-gradient(90deg, rgba(0,174,243,1) 0%, rgba(0,174,243,1) 20%, rgba(145,0,255,1) 50%, rgba(232,27,35,1) 80%, rgba(232,27,35,1) 100%);
   padding: 4px;
   border-radius: 10px;
   margin: 2%;
   transition: 0.5s;
-  animation: ${rotate} 2s linear infinite;
   cursor: pointer;
+  ${'' /* animation: ${rotate} 2s linear infinite; */}
 `;
 
 const ButtonContainer = styled.button`
-  position: relative;
   width: 100%;
-  height: 100%;
-  text-decoration: none;
-  margin: 0;
-  display: inline-block;
-  padding: 10px 120px;
+  padding: 10px 50px;
   color: #FFF;
   letter-spacing: 1px;
-  ${'' /* border: 4px solid ${({ color }) => color}; */}
   background: #333;
+  ${'' /* border: 4px solid ${({ color }) => color}; */}
   ${'' /* background: ${({ color }) => color}; */}
   transition: 0.5s;
   border-radius: 10px;
   cursor: pointer;
-  ${'' /* -webkit-box-reflect: below 1px linear-gradient(transparent, #0003); */}
   :hover {
     ${'' /* background: ${({ color }) => color}B8; */}
     background: ${BackgroundColor}B8;
@@ -46,11 +42,11 @@ const ButtonContainer = styled.button`
 `;
 
 const Button = ({ children, color, onClick }) => (
-  <ButtonGradient color={color} onClick={onClick}>
+  <ButtonWrapper color={color} onClick={onClick}>
     <ButtonContainer color={color}>
       {children}
     </ButtonContainer>
-  </ButtonGradient>
+  </ButtonWrapper>
 );
 
 Button.propTypes = {
