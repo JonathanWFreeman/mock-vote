8f5724d07711b600c13c857eb2cf34aff4c6f909
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-09-28 22:20:41 -0500

    refactor: Updated to new component

--- src/components/pages/Candidate.js
+++ src/components/pages/Candidate.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ImageElement} from '../elements';
+import {ImageSubmit} from '../elements';
 import {FlexContainer} from '../utilities';
 import {DemocratBlue, RepublicanRed} from '../../Global';
 
@@ -8,8 +8,8 @@ const Candidate = () => {
     <>
       <h2>Choose your candidate!</h2>
       <FlexContainer>
-        <ImageElement img={'joe-biden.jpg'} alt={'Joe Biden'} title={'Joe Biden'} color={DemocratBlue} type={'candidate'} choice={'biden'} link={'party'} />
-        <ImageElement img={'donald-trump.jpg'} alt={'Donald Trump'} title={'Donald Trump'} color={RepublicanRed} type={'candidate'} choice={'trump'} link={'party'} />
+        <ImageSubmit img={'joe-biden.jpg'} alt={'Joe Biden'} title={'Joe Biden'} color={DemocratBlue} type={'candidate'} choice={'biden'} link={'party'} />
+        <ImageSubmit img={'donald-trump.jpg'} alt={'Donald Trump'} title={'Donald Trump'} color={RepublicanRed} type={'candidate'} choice={'trump'} link={'party'} />
       </FlexContainer>
     </>
   )
--- src/components/pages/Party.js
+++ src/components/pages/Party.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { FlexContainer } from '../utilities/';
-import { ImageElement } from '../elements';
+import { ImageSubmit } from '../elements';
 
 const Party = () => {
   return (
     <>
       <h2>Choose your party!</h2>
       <FlexContainer>
-          <ImageElement img={'democrat-logo.png'} alt={'Democrat Logo'} title={'Democrat'} type={'party'} choice={'democrat'} link={'submit'} />
-          <ImageElement img={'republican-logo.png'} alt={'Republican Logo'} title={'Republican'} type={'party'} choice={'republican'} link={'submit'} />
-          <ImageElement img={'other-logo.png'} alt={'Independent Logo'} title={'Other'} type={'party'} choice={'other'} link={'submit'} />
+          <ImageSubmit img={'democrat-logo.png'} alt={'Democrat Logo'} title={'Democrat'} type={'party'} choice={'democrat'} link={'submit'} />
+          <ImageSubmit img={'republican-logo.png'} alt={'Republican Logo'} title={'Republican'} type={'party'} choice={'republican'} link={'submit'} />
+          <ImageSubmit img={'other-logo.png'} alt={'Other Logo'} title={'Other'} type={'party'} choice={'other'} link={'submit'} />
       </FlexContainer>
     </>
   )
