9790c1c242a7fab61f59df3a4d5256587fa2e821
Jonathan Freeman <JonathanWFreeman@gmail.com>
2021-08-02 21:58:26 -0500

    fix: Removed firebase auth and replaced with localstorage

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,102 +1,52 @@
-import React, {useContext, useEffect, useState} from 'react'
-import firebase from 'firebase'
-import { useFirebase } from '../firebase'
+import React, {useContext, useState} from 'react'
 import {VoteContext, RouteContext} from '../context'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {Button} from '../elements'
-import {fetchLocation, Below} from '../utilities'
+import {fetchLocation} from '../utilities'
 import {handleError, wait} from '../../helpers'
 
 const Wrapper = styled.section`
   margin: 0 2%;
 `;
 
-const Disclaimer = styled.div`
-  padding: 25px;
-  position: absolute;
-  bottom: 0;
-  ${Below.small`
-    position: initial;
-  `}
-`;
-
 const Login = ({setError, setLoading}) => {
-  const firestore = useFirebase();
   const [userExists, setUserExists] = useState(false);
   const [vote, setVote] = useContext(VoteContext);
   const [route, setRoute] = useContext(RouteContext); 
 
-  async function authenticate(provider) {
+  async function authenticate() {
     setLoading(true);
-    await providerLogin(provider);
+    await checkIfVoted();
     setLoading(false);
   }
-  
-  async function providerLogin(provider) {
-    const authProvider = await new firebase.auth[`${provider}AuthProvider`]();
-    await firestore.auth().signInWithRedirect(authProvider).catch(err => handleError(err.message, setError, setRoute));
-    await firestore.auth().getRedirectResult().then(res => console.log(res)).catch(err => handleError(err.message, setError, setRoute));
-  }
 
-  useEffect(() => {
-    function checkUserExists(firestore, uid) {
-      return new Promise((resolve, reject) => {
-        firestore.users().where('uid', '==', uid).get()
-        .then(function(ref) {
-          ref.empty ? reject('User has not voted') : resolve(true);
-        }).catch((err) => handleError(err, setError, setRoute));   
-      })
-    } 
-    
-    async function authHandler(authData) {
-      if(authData.user !== null){
-        const getUserId = authData.user.uid;
-        const getUserEmail = authData.user.email;          
-        const isUser = await checkUserExists(firestore, getUserId).catch((err) => console.log(err));
-
-        if(isUser === true){
-          setUserExists(true)
-          await wait(3000)
-          setRoute('results')
-          localStorage.setItem('uid', getUserId);
-          setUserExists(false)
+  async function checkIfVoted() {
+    const voted = localStorage.voted; 
+    if(!voted){
+      const userLocation = await fetchLocation().catch((err) => handleError(err, setError, setRoute));
+      if(userLocation){
+        if(userLocation.region_code.length === 0){
+          setError('Error getting region data')
+          setRoute('error')
+        }
+        else if(userLocation.country_code !== 'US'){
+          setError('US residents only')
+          setRoute('error')
         }else{
-          const userLocation = await fetchLocation().catch((err) => handleError(err, setError, setRoute));
-          if(userLocation){
-            if(userLocation.region_code.length === 0){
-              setError('Error getting region data')
-              setRoute('error')
-            }
-            else if(userLocation.country_code !== 'US'){
-              setError('US residents only')
-              setRoute('error')
-            }else{
-              setVote(prevVote => ({...prevVote,
-                state: userLocation.region_code,
-                uid: getUserId,
-                email: getUserEmail,
-              }));       
-              setRoute('candidate')
-            }
-          }
+          setVote(prevVote => ({...prevVote,
+            state: userLocation.region_code,
+          }));       
+          setRoute('candidate')
         }
       }
+    } else {
+      setUserExists(true)
+      await wait(2000)
+      setUserExists(false)
+      setRoute('results')
     }
-    async function doStuff(){
-      setLoading(true);
-      await firestore.auth().getRedirectResult().then(authHandler).catch((err) => handleError(err, setError, setRoute));
-      const isUser = localStorage.getItem('uid');
-      if(isUser){
-        setUserExists(true)
-        await wait(2000)
-        setUserExists(false)
-        setRoute('results')
-      }
-      setLoading(false);
-    }
-    doStuff();
-  }, [firestore, setError, setLoading, setRoute, setVote]);
+  }
 
   if(userExists) {
     return (
@@ -111,12 +61,7 @@ const Login = ({setError, setLoading}) => {
     <>
       <Wrapper>
         <h2>Welcome to Election 2020!</h2>
-        <h3>Please login via Facebook to cast your vote!</h3>
-        <Button onClick={() => authenticate('Facebook')}><p>Login with Facebook</p></Button>
-        <Disclaimer>
-          <p>Facebook is only used to help prevent multiple votes being cast.</p>
-          <p>No personal information is kept.</p>
-        </Disclaimer>
+        <Button onClick={() => authenticate()}><p>Vote</p></Button>
         <Button onClick={() => setRoute('results')}><p>Results</p></Button>
       </Wrapper>
     </>
